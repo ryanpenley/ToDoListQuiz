8d0272718895c2d7063654bd5cfd71b7b3954dce
Ryan Penley <ryanpenley@gmail.com>
2023-02-09 16:21:49 -0500

    added docker support and demo login before pushing to railway

---
+++ Data/DataUtility.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using ToDoListQuiz.Models;
+
+namespace ToDoListQuiz.Data
+{
+    public static class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            string? databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString! : BuildConnectionString(databaseUrl);
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder()
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+            return builder.ToString();
+        }
+
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+
+            // obtains the necessary services based on the IServiceProvider parameter
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+            var userManagerSvc = svcProvider.GetRequiredService<UserManager<AppUser>>();
+
+            // Align the database by checking Migrations
+            await dbContextSvc.Database.MigrateAsync();
+
+
+            // Seed Demo User
+            await SeedDemoUserAsync(userManagerSvc);
+
+        }
+
+        private static async Task SeedDemoUserAsync(UserManager<AppUser> userManager)
+        {
+            AppUser demoUser = new AppUser()
+            {
+                UserName = "demologin@addressbookgpt.com",
+                Email = "demologin@addressbookgpt.com",
+                FirstName = "Demo",
+                LastName = "Login",
+                EmailConfirmed = true
+            };
+
+            try
+            {
+                AppUser? user = await userManager.FindByEmailAsync(demoUser.Email);
+
+                if (user == null)
+                {
+                    await userManager.CreateAsync(demoUser, "Abc&123!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("**********  ERROR *************");
+                Console.WriteLine("Error Seeding Demo Login User.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("********************************");
+
+                throw;
+            }
+        }
+
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using ToDoListQuiz.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = DataUtility.GetConnectionString(builder.Configuration) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -21,6 +21,9 @@ builder.Services.AddMvc();
 
 var app = builder.Build();
 
+var scope = app.Services.CreateScope();
+
+await DataUtility.ManageDataAsync(scope.ServiceProvider);
 
 
 
